2b20058da6f3a6ee88c8574174d2e2249942a78f
Pawinnarut Pornpanarat <pawinnarut.dev@gmail.com>
2026-01-29 11:03:59 +0700

    Add models for Company and PreviousJob, update admin and landing page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from main.models import PreviousJob, Company
+
 
 # Register your models here.
+admin.site.site_header = "Admin Dashboard"
+admin.site.site_title = "Admin Dashboard"
+admin.site.index_title = "Welcome to the Admin Dashboard"
+admin.site.register(PreviousJob)
+admin.site.register(Company)
+
